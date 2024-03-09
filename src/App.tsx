583d8198be30a993c67e60e760c5976a2252a439
Professor1345 <alfulaani1345@gmail.com>
2024-03-09 09:05:54 +0100

    BG image ishh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import TranslateInput from "./components/Translate Input";
 import TranslateOutput from "./components/Translate Output";
 
 const App = () => {
   return (
-    <div className="min-h-screen font-DM-Sans bg-[url('./src/assets/images/hero_img.jpg')] bg-[#040711] bg-contain bg-no-repeat bg-fixed bg-top flex flex-col justify-center items-center">
+    <div className="min-h-screen font-DM-Sans bg-[url('./assets/images/hero_img.jpg')] bg-[#040711] bg-contain bg-no-repeat bg-fixed bg-top flex flex-col justify-center items-center">
       <div className="my-5">
         <img src="/logo.svg" alt="translated.io" />
       </div>
